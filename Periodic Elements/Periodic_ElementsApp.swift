7133099820c18736f02c24cc5d2e77959a37c924
Jack Chevalier <johnchevalier934@yahoo.com>
2021-02-25 10:18:25 -0600

    Initial Commit

---
+++ Periodic Elements/Periodic_ElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Periodic_ElementsApp.swift
+//  Periodic Elements
+//
+//  Created by Student on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Periodic_ElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
